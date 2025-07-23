7d4bdab35fef1d8e43cdbbce76337d07d5426cff
jatin251212 <jatinkantariya789@gmail.com>
2025-07-23 20:17:41 +0530

    feat: integrate schematic with stripe and our app

---
+++ actions/getTemporaryAccessToken.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { currentUser } from "@clerk/nextjs/server";
+
+import { SchematicClient } from "@schematichq/schematic-typescript-node";
+
+const apiKey = process.env.SCHEMATIC_API_KEY;
+
+const client = new SchematicClient({ apiKey });
+
+export async function getTemporaryAccessToken() {
+  console.log("Getting temporary Access Token");
+  const user = await currentUser();
+  if (!user) {
+    console.log("No use found");
+    return null;
+  }
+
+  console.log(`Issuing temporary Access token to ${user.id}`);
+
+  const resp = await client.accesstokens.issueTemporaryAccessToken({
+    resourceType: "company",
+    lookup: { id: user.id },
+  });
+  console.log(
+    "Token response recieved:",
+    resp.data ? "Token recieved" : "Token not received",
+  );
+}
