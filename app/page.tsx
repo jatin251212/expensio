d2fd4bdc823caeb9e153acde0fe27fcb7f0a0d24
jatin251212 <jatinkantariya789@gmail.com>
2025-07-23 13:40:52 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className="text-4xl font-bold text-center">
+      <h1>Header</h1>
+    </div>
+  );
+}
+
+export default Header;
